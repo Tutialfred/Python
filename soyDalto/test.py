7e9610adfb4810ce73f2908d1fe8404bf62cb494
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-05-15 22:48:04 -0300

    Python .

--- soyDalto/test.py
+++ soyDalto/test.py
@@ -1,12 +1,9 @@
 
 
 
-# diccionario1 = dict(name="tuti", business="Clothes or UNDEFINED")
-diccionario = {frozenset(["Name", "Nombre", "nome"]) : "Oscar Alfredo"}
 
-diccionario = dict.fromkeys(["Nombre", "edad", "Mail", "Numero"])
-diccionario = dict.fromkeys(["Name", "lastname", "number"], "sin-completar")
-print(diccionario)
+
+
 
 
 
